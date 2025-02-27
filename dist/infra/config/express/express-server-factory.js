172ff2b80894be95e35e599c73a4b861fd5a48d0
EnzoDPrado <enzodp1501@gmail.com>
2025-02-27 18:03:59 -0300

    chore: removing build

--- dist/infra/config/express/express-server-factory.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.expressServerFactory = void 0;
-const express_server_1 = require("./express.server");
-const expressServerFactory = () => express_server_1.ExpressServer.getServer();
-exports.expressServerFactory = expressServerFactory;
--- dist/infra/config/knex/knex-server-factory.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.knexServerFactory = void 0;
-const knex_server_1 = require("./knex-server");
-const knexServerFactory = () => knex_server_1.KnexServer.getServer();
-exports.knexServerFactory = knexServerFactory;
