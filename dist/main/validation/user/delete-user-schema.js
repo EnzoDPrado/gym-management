cf8161b5c34929eb8f1f4c2f133367ad60e52f48
EnzoDPrado <enzodp1501@gmail.com>
2025-02-26 21:09:33 -0300

    feat: removing build file and renaming server to index

--- dist/main/validation/user/delete-user-schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteUserSchema = void 0;
-const zod_1 = require("zod");
-exports.deleteUserSchema = zod_1.z.object({
-    user_id: zod_1.z.number()
-});
